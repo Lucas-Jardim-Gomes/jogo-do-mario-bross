bf9c2c613116ca3c50cc957e29684e873106365c
Lucasdev07 <113186715+Lucasdev07@users.noreply.github.com>
2023-02-24 17:59:56 -0300

    atualização

--- JS/jogo-mario-bross.js
+++ JS/jogo-mario-bross.js
@@ -1,31 +1,74 @@
-       /*  var direita ;
-        var esquerda;
-        var velocidade;
-        var objeto;
-        var tempo;
-        function iniciar(){
-            direita=0;
-            esquerda=0;
-            velocidade= 10;
-            objeto = document.getElementsById('mario-gif');
-            document.addEventListener("keydown", 0);
-            document.addEventListener("keyup", 0);
-            tempo = setInterval(0, 20);
-        }
+/*  var direita ;
+ var esquerda;
+ var velocidade;
+ var objeto;
+ var tempo;
+ function iniciar(){
+     direita=0;
+     esquerda=0;
+     velocidade= 10;
+     objeto = document.getElementsById('mario-gif');
+     document.addEventListener("keydown", 0);
+     document.addEventListener("keyup", 0);
+     tempo = setInterval(0, 20);
+ }
 
-        window.addEventListener("load",iniciar); */
-        var canva = document.querySelector ("canvas");
-        var contexto = canva.getContext("2d");
-        var cenario = new Image();
-        function IniciarJogo(){
-            cenario.src = "../imagens/SNES - Super Mario World - Yoshis Island 1.png"
-            CriaCenario();
-        }
+ window.addEventListener("load",iniciar);
+ var canva = document.querySelector ("canvas");
+ var contexto = canva.getContext("2d");
+ var cenario = new Image();
+ function IniciarJogo(){
+     cenario.src = "../imagens/SNES - Super Mario World - Yoshis Island 1.png"
+     CriaCenario();
+ }
 
-        function CriaCenario(){
-            requestAnimationFrame(CriaCenario);
-            canva.drawImage(cenario,0,0,800,600);
-        }
+ function CriaCenario(){
+     requestAnimationFrame(CriaCenario);
+     canva.drawImage(cenario,0,0,800,600);
+ }
 
-        CriaCenario();
-        
+ CriaCenario();
+  */
+
+/*
+var x = 0
+var y = 0
+window.addEventListener("keydown", function(event) {
+    var tecla = event.keyCode
+if ( tecla == "39") { 
+    x = x + 5
+this.document.getElementById("cenario").style.left=x+"px"
+}
+if ( tecla == "37") { 
+    x = x - 5
+this.document.getElementById("cenario").style.left=x+"px"
+}
+})
+
+*/
+(function() {
+    var cnv = document.querySelector('canvas');
+    var ctx = cnv.getContext('2d');
+
+    //recursos do jogo
+    //var cenario = new Image();
+    //cenario.src = "cenario1-mario.png"
+    const cenario = new Image(); // Image constructor
+cenario.src = 'cenario-mario.png';
+
+    function loop(){
+        window.requestAnimationFrame(loop,cnv);
+        update();
+        render();
+    }
+
+    function update(){
+
+    }
+
+    function render(){
+        ctx.drawImage(cenario,0,0,5120,432,0,0,5120,432);
+    }
+
+    loop();
+}());
